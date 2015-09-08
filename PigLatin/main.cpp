5d280203a46dbf59d195c4826fde620e3eb2d57d
Mikerah <Mikerah1@users.nonreply.github.com>
2015-09-08 09:05:45 -0400

    Pig Latin Added.

--- PigLatin/main.cpp
+++ PigLatin/main.cpp
@@ -3,28 +3,50 @@
 
 using namespace std;
 
+bool findSubString(string s1, string s2);
+
 int main()
 {
-    string vowels = "eaiou";
+    char vowels[5] = {'e', 'a','i','u','0'};
     string word;
+
+
 
     cout << "Enter a word and this program will output the pig latin version of it: ";
     cin >> word;
 
-    string firstChar = word.substr(0,1);
+    char firstChar = tolower(word[0]);
+    string firstCharStr = word.substr(0,1);
 
-    if(vowels.find(firstChar) || firstChar == "e")
+    if(firstChar == 'e' || firstChar == 'a' || firstChar == 'i' || firstChar == 'u' || firstChar == 'o')
     {
         word.insert(word.length(),"yay");
         cout << word;
     }
     else
     {
-        word.append(firstChar);
+        word.append(firstCharStr);
         word.append("ay");
         word.erase(0,1);
         cout << word;
     }
+
+
+//    for (int i = 0; i < sizeof(vowels); i++)
+//    {
+//        if (vowels[i] == firstCharStr)
+//        {
+//            word.insert(word.length(),"yay");
+//            cout << word;
+//            break;
+//        }
+//
+//        word.append(firstCharStr);
+//        word.append("ay");
+//        word.erase(0,1);
+//        cout << word;
+//        break;
+//    }
 
 
     return 0;
